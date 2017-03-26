f44024684b9e9a73f517bfd847252c99383ac0d9
Eduard Dumitru <eduard_dum@mail.com>
2017-03-26 19:34:48 +0300

    making rsa encryption in server side

--- connection.js
+++ connection.js
@@ -1,11 +1,31 @@
+var crypto = require('crypto');
+var ursa = require('ursa');
+
+var KEY_BITS = 4096;
+var PBKDF2_SALT_BITS = 256;
+var PBKDF2_ITERATIONS = 20000;
+var PBKDF2_LENGTH_BITS = 256;
+
 module.exports = function (io) {
   io.sockets.on('connection', (socket) => {
     console.log('user connected')
 
-    socket.emit('userName');
+    //socket.emit('userName');
 
-    socket.on('hello', (msg) => {
-      console.log('message: ' + msg);
+    socket.on('conn_request', (rcv_keypair) => {
+      console.log ('received user public key : ');
+      console.log (rcv_keypair);
+      socket.client_pubkey = rcv_keypair.user_pub_key;
+      socket.server_keypair = ursa.generatePrivateKey(KEY_BITS);
+      socket.emit('conn_acknowledge', {svr_pub_key : socket.server_keypair.toPublicPem('utf8')});
+    });
+
+    socket.on('hash_info', (hash_info) => {
+      console.log ('received encrypted user hash_info : ');
+      console.log (hash_info);
+      var privateKey = ursa.coercePrivateKey(socket.server_keypair.toPrivatePem('utf8'));
+      socket.client_hashinfo = privateKey.decrypt(encData, 'base64', 'utf8');
+      socket.emit('conn_acknowledge', {svr_pub_key : socket.server_keypair.toPublicPem('utf8')});
     });
 
     socket.on('disconnect', () => {
